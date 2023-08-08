19931335dc373b72c63908e6b421b56f8a9fec45
Ejimmi <jimohwasiuejimmi@gmail.com>
2023-08-08 10:17:06 +0000

    js-practices

---
+++ myscript.js
@@ -0,0 +1,45 @@
+
+// Student names
+const student1 = 'Jimoh';
+const student2 = 'Enoch';
+const student3 = 'Aaron';
+const student4 = 'Nissi';
+const student5 = 'Davis';
+const student6 = 'Mary';
+const student7 = 'Richmond';
+const student8 = 'Ablashika';
+const student9 = 'Governor';
+const student10 = 'Solomon';
+
+// Student ages
+let Jimoh = 25;
+let Enoch = 30;
+let Aaron = 35;
+let Nissi = 36;
+let Davis = 18;
+let Mary = 10;
+let Richmond = 22;
+let Ablashika = 40;
+let Governor = 19;
+let Solomon = 15;
+
+// Calculate the total age
+let agetotal = Jimoh + Enoch + Aaron + Nissi + Davis + Mary + Richmond + Ablashika + Governor + Solomon;
+
+console.log(agetotal);
+
+// Display age and names of all students
+console.log(`${student1}: ${Jimoh} years old`);
+console.log(`${student2}: ${Enoch} years old`);
+console.log(`${student3}: ${Aaron} years old`);
+console.log(`${student4}: ${Nissi} years old`);
+console.log(`${student5}: ${Davis} years old`);
+console.log(`${student6}: ${Mary} years old`);
+console.log(`${student7}: ${Richmond} years old`);
+console.log(`${student8}: ${Ablashika} years old`);
+console.log(`${student9}: ${Governor} years old`);
+console.log(`${student10}: ${Solomon} years old`);
+
+console.log(`Total age of all students: ${agetotal} years`);
+
+document.write(agetotal)
